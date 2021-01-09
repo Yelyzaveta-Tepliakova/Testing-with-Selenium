ac05fb87f20f277afe0cfebf00bc6fa9df2de638
Yelyzaveta Tepliakova <l1zza.t1999@gmail.com>
2021-01-09 15:53:25 +0200

    Code style was updated

--- tests/selen_testing_youtube.py
+++ tests/selen_testing_youtube.py
@@ -17,6 +17,39 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 
+HTML_YOUTUBE_BUTTONS = {"button_1":  '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/'
+                                     'div[1]/div/div[5]/div[2]/ytd-video-primary-info-renderer/div/div/'
+                                     'div[3]/div/ytd-menu-renderer/div/ytd-toggle-button-renderer[1]/'
+                                     'a/yt-icon-button/button/yt-icon',
+                        "button_2":  '/html/body/ytd-app/ytd-popup-container/iron-dropdown/div/'
+                                     'ytd-modal-with-title-and-button-renderer/yt-formatted-string[2]',
+                        "button_3":  '/html/body/ytd-app/ytd-popup-container/iron-dropdown/div/'
+                                     'ytd-modal-with-title-and-button-renderer/yt-formatted-string[2]',
+                        "button_4":  '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/'
+                                     'div[1]/div/div[5]/div[2]/ytd-video-primary-info-renderer/div/div/'
+                                     'div[3]/div/ytd-menu-renderer/div/ytd-toggle-button-renderer[1]/a/'
+                                     'yt-icon-button/button/yt-icon',
+                        "button_5":  '/html/body/ytd-app/ytd-popup-container/iron-dropdown/div/'
+                                     'ytd-modal-with-title-and-button-renderer/div/ytd-button-renderer'
+                                     '/a/paper-button/yt-formatted-string',
+                        "button_6":  'https://accounts.google.com/ServiceLogin?service=youtube&uilel=' 
+                                     '3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3' 
+                                     'Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Duk%26next%3D%252' 
+                                     'Fwatch%253Fv%253DD08L2JJB1ag&hl=uk&ec=66426'
+                                     'ytd-modal-with-title-and-button-renderer/yt-formatted-string[2]',
+                        "button_7":  '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/'
+                                     'div[1]/div/div[6]/div[3]/ytd-video-secondary-info-renderer/div/'
+                                     'div[2]/ytd-video-owner-renderer/a/yt-img-shadow/img',
+                        "button_8":  '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/'
+                                     'div[1]/div/div[6]/div[3]/ytd-video-secondary-info-renderer/div/'
+                                     'div[2]/ytd-video-owner-renderer/a/yt-img-shadow/img',
+                        "button_9":  '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/'
+                                     'ytd-c4-tabbed-header-renderer/app-header-layout/div/app-header/'
+                                     'div[2]/div[2]/div/div[2]/div[2]/a[3]/yt-img-shadow/img'
+
+                        }
+
+
 if os.path.isfile("YouTube_testing_results.txt") is True:  # creates file for results of YouTube_test
     os.remove("YouTube_testing_results.txt")
 with open("YouTube_testing_results.txt", 'w') as doc:
@@ -46,17 +79,14 @@ class YouTubeTesting(unittest.TestCase):
         to like video if user is not signed in
         :return: None
         """
+
         name = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/'
-                                                  'div[1]/div/div[5]/div[2]/ytd-video-primary-info-renderer/div/div/'
-                                                  'div[3]/div/ytd-menu-renderer/div/ytd-toggle-button-renderer[1]/'
-                                                  'a/yt-icon-button/button/yt-icon')))
+            EC.element_to_be_clickable((By.XPATH, HTML_YOUTUBE_BUTTONS["button_1"])))
         name.click()
         name = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '/html/body/ytd-app/ytd-popup-container/iron-dropdown/div/'
-                                                      'ytd-modal-with-title-and-button-renderer/yt-formatted-string[2]')))
-        name = self.driver.find_element_by_xpath("/html/body/ytd-app/ytd-popup-container/iron-dropdown/div/"
-                                                 "ytd-modal-with-title-and-button-renderer/yt-formatted-string[2]").text
+            EC.presence_of_element_located((By.XPATH, HTML_YOUTUBE_BUTTONS["button_2"])))
+        name = self.driver.find_element_by_xpath((By.XPATH, HTML_YOUTUBE_BUTTONS["button_3"])).text
+
         try:
             assert name == "Увійдіть, щоб ми могли врахувати вашу думку."
             print("Test \"test1_inability_to_like_video_if_not_signed_in\" passed")
@@ -74,21 +104,13 @@ class YouTubeTesting(unittest.TestCase):
         :return: None
         """
         name = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/'
-                                                  'div[1]/div/div[5]/div[2]/ytd-video-primary-info-renderer/div/div/'
-                                                  'div[3]/div/ytd-menu-renderer/div/ytd-toggle-button-renderer[1]/a/'
-                                                  'yt-icon-button/button/yt-icon')))
+            EC.element_to_be_clickable((By.XPATH, HTML_YOUTUBE_BUTTONS["button_4"])))
         name.click()
         name = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/ytd-app/ytd-popup-container/iron-dropdown/div/'
-                                                  'ytd-modal-with-title-and-button-renderer/div/ytd-button-renderer'
-                                                  '/a/paper-button/yt-formatted-string')))
+            EC.element_to_be_clickable((By.XPATH, HTML_YOUTUBE_BUTTONS["button_5"])))
         name.click()
         try:
-            assert self.driver.current_url == "https://accounts.google.com/ServiceLogin?service=youtube&uilel=" \
-                                              "3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3" \
-                                              "Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Duk%26next%3D%252" \
-                                              "Fwatch%253Fv%253DD08L2JJB1ag&hl=uk&ec=66426"
+            assert self.driver.current_url == HTML_YOUTUBE_BUTTONS["button_6"]
             print("Test \"test2_pressing_button_to_sign_in\" passed")
             with open("test_YouTube.txt", 'a') as doc:
                 doc.write("\nTest case \"test2_pressing_button_to_sign_in\" passed")
@@ -104,9 +126,7 @@ class YouTubeTesting(unittest.TestCase):
         :return: None
         """
         name = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/'
-                                                  'div[1]/div/div[6]/div[3]/ytd-video-secondary-info-renderer/div/'
-                                                  'div[2]/ytd-video-owner-renderer/a/yt-img-shadow/img')))
+            EC.element_to_be_clickable((By.XPATH, HTML_YOUTUBE_BUTTONS["button_7"])))
         name.click()
         try:
             assert self.driver.current_url == "https://www.youtube.com/user/novychannel"
@@ -128,14 +148,10 @@ class YouTubeTesting(unittest.TestCase):
 
         # checks button going to channel and opening instagram
         name = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/'
-                                                  'div[1]/div/div[6]/div[3]/ytd-video-secondary-info-renderer/div/'
-                                                  'div[2]/ytd-video-owner-renderer/a/yt-img-shadow/img')))
+            EC.element_to_be_clickable((By.XPATH, HTML_YOUTUBE_BUTTONS["button_8"])))
         name.click()
         name = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/'
-                                                  'ytd-c4-tabbed-header-renderer/app-header-layout/div/app-header/'
-                                                  'div[2]/div[2]/div/div[2]/div[2]/a[3]/yt-img-shadow/img')))
+            EC.element_to_be_clickable((By.XPATH, HTML_YOUTUBE_BUTTONS["button_9"])))
         name.click()
         try:
             assert len(self.driver.window_handles) == 2
